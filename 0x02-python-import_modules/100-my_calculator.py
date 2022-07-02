dd6c74b0204f4c192c23b01a4a05e1a2599fe1af
billyotieno <billybrian94@gmail.com>
2022-07-02 09:58:27 -0700

    added: script my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,22 +8,22 @@ def mycalculator(argv):
     if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-
-    if op == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        a = int(argv[1])
+        op = argv[2]
+        b = int(argv[3])
+
+        if op == '+':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+        elif op == '-':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+        elif op == '*':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+        elif op == '/':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
 
 
 if __name__ == "__main__":
