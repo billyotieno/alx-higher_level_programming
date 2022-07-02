c1330a78a0f4ddcffeaac02bb300d2b8908524c9
billyotieno <billybrian94@gmail.com>
2022-07-02 12:45:08 -0700

    added: matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) == 0:
+        print("")
+    else:
+        for i in matrix:
+            for j in i:
+                print(j, end=' ')
+            print("")
