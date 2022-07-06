a271b81518469c2681e0df83dbafec626171daac
Billy Otieno <billybrian94@gmail.com>
2022-07-06 23:57:22 +0300

    Updated: Square matrix simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for index, array in enumerate(matrix):
-        for item_index, item in enumerate(array):
-            matrix[index][item_index] = item ** 2
-    return matrix
+    new = []
+    for x in matrix:
+        new.append(list(map(lambda x: x**2, x)))
+    return (new)
